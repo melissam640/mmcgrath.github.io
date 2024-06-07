34dc0fa39f3db325c27104d130043aa4cfc12e76
Melissa McGrath <melissam640@gmail.com>
2024-06-07 15:32:33 -0700

    Added nav bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react'
-import { Fade } from "react-awesome-reveal"
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import MyButton from './MyButton.jsx'
+import { useState, useEffect } from 'react';
+import { Fade } from "react-awesome-reveal";
+import './App.css';
+import MyButton from './MyButton.jsx';
+import NavBar from '../components/NavBar.jsx';
+import StickyScroll from '../components/ScrollingBio.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -22,27 +23,23 @@ function App() {
 
   return (
     <>
-      <Fade>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-      <h1>Welcome!</h1>
-      <p>I'm Melissa, a former senior project engineer turned software developer.</p>
-      </Fade>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <h3>{ currentTime }</h3>
+      <div className="app-container">
+        <header>
+          <NavBar />
+        </header>
+        
+        <Fade>
+        <h1>Welcome!</h1>
+        <p>I'm Melissa, a former senior project engineer turned software developer.</p>
+        </Fade>
+        <MyButton message="Start" />
+        <MyButton message="End" />
+        <Fade>
+          <p>I will gently appear as I enter the viewport</p>
+        </Fade>
+        {/* <StickyScroll /> */}
       </div>
-      <MyButton message="Start" />
-      <MyButton message="End" />
-      <Fade>
-        <p>I will gently appear as I enter the viewport</p>
-      </Fade>
+      
     </>
   )
 }
